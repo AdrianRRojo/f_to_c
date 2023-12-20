0b9d250fad4c1c89777b6b822634295514e2bc57
Adrian Rojo <rojoadrian16@gmail.com>
2023-12-20 00:48:39 -0500

    Removed comments

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::io;
 
 fn main() {
     println!("Hello welcome to my Fahrenheit to Celsius converter in the rust programming language");
-    // The math: C = (F - 32) × 5/9
-    // F = C × (9/5) + 32
+    
 
     // define the users input
     let mut temp = String::new();
@@ -30,23 +29,19 @@ fn main() {
     //remove trailing whitespace after the entry. 
     let converter = converter.trim();
 
-    // match converter.as_str() {
-    //     "F" => let _answer = new_variable - 32 * 5/9,
-    //     "C" => println!("C"),
-    //     _ => println!("Please enter F or C"),
-    // }
-        if converter == "F"{
-            // println!("ANS: My F");
-            let _answer = (new_variable - 32) * 5/9;
-            println!("{_answer}°C");
-        } else if converter == "C"{
-            // println!("ANS: {converter}");
-            let _answer = new_variable * 9/5 + 32;
-            println!("{_answer}°F");
-        }else{
-            println!("Please enter F or C");
-        }
-    // println!("your temp is {temp}");
+    // The math: 
+    // C = (F - 32) × 5/9
+    // F = C × (9/5) + 32
+    if converter == "F"{
+        // println!("ANS: My F");
+        let _answer = (new_variable - 32) * 5/9;
+        println!("{_answer}°C");
+    } else if converter == "C"{
+        // println!("ANS: {converter}");
+        let _answer = new_variable * 9/5 + 32;
+        println!("{_answer}°F");
+    }else{
+        println!("Please enter F or C");
+    }
 
-    // println!("your temp is {_answer}");
 }
